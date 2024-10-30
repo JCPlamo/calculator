47bf113d8718a6907d4815b2f25cdba12729c35b
JCPlamo <justincplamo@gmail.com>
2024-10-30 15:10:25 -0500

    Create global variables & create operate function

--- script.js
+++ script.js
@@ -19,8 +19,33 @@ function divide(num1, num2)
     return num1 / num2;
 }
 
+var firstNum;
+var operator = "";
+var secondNum;
+
+function operate(num1, operator, num2)
+{
+    if(operator === "+")
+    {
+        return add(num1,num2);
+    }
+    else if(operator === "-")
+    {
+        return subtract(num1,num2);
+    }
+    else if(operator === "*")
+    {
+        return multiply(num1,num2);
+    }
+    else if(operator === "/")
+    {
+        return divide(num1,num2);
+    }
+
+}
+
 //Testing
-console.log(add(5,5));
-console.log(subtract(5,5));
-console.log(multiply(5,5));
-console.log(divide(5,5));
+console.log(operate(1,"+",1))
+console.log(operate(1,"-",1))
+console.log(operate(1,"*",1))
+console.log(operate(1,"/",1))
