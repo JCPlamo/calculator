c90ff73f67c53a2fdd57b733e26405d36b7fa7d9
JCPlamo <justincplamo@gmail.com>
2024-10-30 15:01:37 -0500

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,26 @@
+//Math functions
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function subtract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+function divide(num1, num2)
+{
+    return num1 / num2;
+}
+
+//Testing
+console.log(add(5,5));
+console.log(subtract(5,5));
+console.log(multiply(5,5));
+console.log(divide(5,5));
