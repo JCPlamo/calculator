5f95d33f3d448fc9f041ab82e90dec62844473f6
JCPlamo <justincplamo@gmail.com>
2024-10-31 09:51:12 -0500

    Add function to display numbers when clicked

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function divide(num1, num2)
     return num1 / num2;
 }
 
-var firstNum;
+var firstNum = "";
 var operator = "";
-var secondNum;
+var secondNum = "";
 
 function operate(num1, operator, num2)
 {
@@ -47,13 +47,22 @@ function operate(num1, operator, num2)
 //Populates display when clicking buttons
 function populateDisplay()
 {
+    
+
     const buttons = document.querySelector(".buttons"); // Get the buttons container
+    const display = document.querySelector(".display");
     //Create buttons for the numbers
     for (let i = 0; i < 10; i++) 
     {
         const numbers = document.createElement("button");
         numbers.classList.add("numbers");
         numbers.textContent = i;
+
+        //Listeners to make the buttons display on screen when clicked
+        numbers.addEventListener('click', () => {
+            firstNum += i;
+            display.textContent = firstNum;
+        });
 
         // Insert the number button at the beginning of the buttons container
         buttons.insertBefore(numbers, buttons.firstChild);
